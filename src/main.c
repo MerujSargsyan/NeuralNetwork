8e8b1fe0756856eb8fbd90f6c72dcdf0711fb333
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-06-21 15:54:53 -0700

    cost function setup

--- src/main.c
+++ src/main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define test_len (int)(sizeof(test) / sizeof(test[0]))
+
 float test[][2] = {
     {1, 2},
     {2, 4},
@@ -10,11 +12,22 @@ float test[][2] = {
     {5, 10},
 };
 
+float cost(float guess) {
+    float result = 0.0f;
+    for(int i = 0; i < test_len; i++) {
+        float diff = test[i][1] - guess * test[i][0];
+        result += diff*diff;
+    }
+    return result;
+}
+
+
 int main(void) {
     srand(69);
     float peram1 = (float) rand() / (float) RAND_MAX;
     peram1 *= 10.0f;
-    printf("%f\n", peram1);
+
+    printf("%f\n", cost(peram1)); 
 
     return 0;
 }
