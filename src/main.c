2637f648b65d9c8c9403befaefcac940f5324b33
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-06-22 13:12:27 -0700

    function pointer used for general operation

--- src/main.c
+++ src/main.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define test_len (int)(sizeof(test) / sizeof(test[0]))
 
 // this does not work for adding
-float test[][4] = {
-    {1, 1, 1, 5},
-    {2, 1, 3, 7},
-    {3, 4, 8, 18},
-    {4, 2, 5, 14},
-    {0, 2, 7, 6},
-    {4, 0, 0, 8}
+float test[][5] = {
+    {0, 0, 0}, 
+    {1, 1, 1},
+    {0, 1, 0},
+    {1, 0, 0},
 };
+int param_size = 2;
 
-int param_size = 3;
+float sum(float a, float b) {
+    return a + b;
+}
+float mult(float a, float b) {
+    return a * b;
+}
 
 // isolate index of current guess
-float cost(float* params, int idx, float eps) {
+float cost(float* params, int idx, float eps, float (*operation)(float, float)) {
     float result = 0.0f;
     for(int i = 0; i < test_len; i++) {
         float total = 0.0f;
@@ -24,7 +29,7 @@ float cost(float* params, int idx, float eps) {
             if(j == idx) {
                 current += eps;
             }
-            total += current * test[i][j];
+            total += operation(current, test[i][j]);
         }
         float diff = test[i][param_size] - total;
         result += diff*diff;
@@ -54,16 +59,15 @@ void print_perams(float* perams) {
 
 int main(void) {
     srand(69);
-    int cell_num = 3;
-    float* perams = generate_perams(cell_num);
+    float* perams = generate_perams(param_size);
 
     float eps = 1e-3;
     float lrn_rate = 1e-2;
 
     // approcimate gradient using finite difference
     for(int i = 0; i < 500; i++) {
-        for(int j = 0; j < cell_num; j++) {
-            float dperam = (cost(perams, j, eps) - cost(perams, -1, eps)) / eps;
+        for(int j = 0; j < param_size; j++) {
+            float dperam = (cost(perams, j, eps, sum) - cost(perams, -1, eps, sum)) / eps;
             perams[j] -= lrn_rate * dperam;
         }
     }
