509bf96d22d33db3d0ac13935ef0d6057f882f5a
MeruzhanS <sargsyanmeruzhan48@gmail.com>
2024-06-22 08:54:30 -0700

    second cell added

--- src/main.c
+++ src/main.c
@@ -1,41 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #define test_len (int)(sizeof(test) / sizeof(test[0]))
 
 // this does not work for adding
-float test[][2] = {
-    {1, 0.25},
-    {2, 0.50},
-    {3, 0.75},
-    {4, 1},
-    {8, 2},
-    {16, 4}
+float test[][3] = {
+    {1, 1, 5},
+    {2, 1, 7},
+    {3, 4, 18},
+    {4, 2, 14},
+    {0, 2, 6},
+    {4, 0, 8}
 };
 
-float cost(float guess) {
+float cost(float guess1, float guess2) {
     float result = 0.0f;
     for(int i = 0; i < test_len; i++) {
-        float diff = test[i][1] - (guess * test[i][0]);
+        float diff = test[i][2] - (guess1 * test[i][0] + guess2 * test[i][1]);
         result += diff*diff;
     }
     return result / test_len;
 }
 
+float rand_float() {
+    float output = (float) rand() / (float) RAND_MAX;
+    return output * 10.0f;
+}
+
 int main(void) {
     srand(69);
-    float peram1 = (float) rand() / (float) RAND_MAX;
-    peram1 *= 10.0f;
+    float peram1 = rand_float();
+    float peram2 = rand_float();
 
     float eps = 1e-3;
     float lrn_rate = 1e-2;
 
     // approcimate gradient using finite difference
     for(int i = 0; i < 200; i++) {
-        float dperam = (cost(peram1 + eps) - cost(peram1))/eps;
-        peram1 -= lrn_rate * dperam;
+        float dperam1 = (cost(peram1 + eps, peram2) - cost(peram1, peram2))/eps;
+        float dperam2 = (cost(peram1, peram2 + eps) - cost(peram1, peram2))/eps;
+        peram1 -= lrn_rate * dperam1;
+        peram2 -= lrn_rate * dperam2;
     }
-    printf("%f\n", peram1);
+    printf("%f, %f\n", peram1, peram2);
 
     return 0;
 }
